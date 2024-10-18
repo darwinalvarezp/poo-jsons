2b5213b553e0404eca2848cf9ba0372d716c382c
darwin alvarez <dalvarezp.1@gmail.com>
2024-10-17 20:52:44 -0500

    jsons

---
+++ testIO/src/main/java/com/programacion/Main.java
@@ -0,0 +1,34 @@
+package com.programacion;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        File file = new File("src/main/resources/datos/empleados.json");
+//        System.out.println("Does it exist? " + file.exists());
+//        System.out.println("The file has " + file.length() + " Bites");
+//        System.out.println("Can file read? " + file.canRead());
+//        System.out.println("Can it be written? " + file.canWrite());
+//        System.out.println("It it a direcctory? " + file.isDirectory());
+//        System.out.println("It it a file? " + file.isFile());
+//        System.out.println("It is a absolute? " + file.isAbsolute());
+//        System.out.println("It is a hidden? " + file.isHidden());
+//        System.out.println("absolute pat " + file.getAbsolutePath());
+//        System.out.println("Las modified on " + new Date(file.lastModified()));
+//
+//        try {
+//            Scanner sc = new Scanner(file);
+//            System.out.println("Leyendo un archivo");
+//            while (sc.hasNext()){
+//                System.out.println(sc.nextLine());
+//            }
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+
+    }
+}
